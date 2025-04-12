db686fbb2bd87f7120f4569619e8b5409305bb1e
Pavlo Maksymov <maksimovpb@gmail.com>
2025-04-12 13:16:17 +0200

    chore: goimports and code formatting

--- cmd/currency-converter/main.go
+++ cmd/currency-converter/main.go
@@ -10,12 +10,11 @@ import (
 
 func main() {
 	err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    exchangeRateApiKey := os.Getenv("EXCHANGE_RATE_API_KEY")
+	exchangeRateApiKey := os.Getenv("EXCHANGE_RATE_API_KEY")
 
 	fmt.Println("Exchange Rate API Key:", exchangeRateApiKey)
 }
-
